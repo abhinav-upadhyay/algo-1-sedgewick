470bae11a229434c40450d75e3f0662ed1d48005
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2012-09-05 02:11:49 +0530

    Make it compile and ready for the tests

--- unionFind/Percolation.java
+++ unionFind/Percolation.java
@@ -1,77 +1,101 @@
+/*
+ * Abhinav Upadhyay
+ * September 5th, 2012
+ * Percolation API Implementation.
+ */
+
 public class Percolation {
-	private WeightedQuickUnionUF qf;
-	private int size;
-	private int grid[][];
-	
-	public Percolation(final int N) {
-		this.size = N;
-		this.qf = new WeightedQuickUnionUF(N * N + 2);
-		int i = 0;
-		this.grid = new int[N][N];
-		
-		for (i = 0; i < N; i++)
-			for (int j = 0; j < N; j++)
-				this.grid[i][j] = 0;
-	}
-	
-	private int translateIndex(final int i, final int j) {
-		int N = this.size;
-		return (i * N + j);
-	}
-	
-	private void checkIndex(final int i, final int j) {
-		int n = this.size;
-		if (i < 1 || i > n)
-			throw new IndexOutOfBoundsException(i + " " + j);
-		
-		if (j < 1 || j > n)
-			throw new IndexOutOfBoundsException(i + " " + j);
-	}
-	
-	public void open(int i, int  j) {
-		checkIndex(i, j);
-		i--;
-		j--;
-		this.grid[i][j] = 1;
-		int N = this.size;
-		if (i == 0) {
-			this.qf.union(translateIndex(i, j), N * N + 1);
-		} else if (i == N - 1) {
-			this.qf.union(translateIndex(i, j), N * N);
-		}
-		
-		if (i > 0 && this.grid[i - 1][j] == 1)
-			this.qf.union(translateIndex(i - 1, j), translateIndex(i, j));
-		
-		if (j > 0 && this.grid[i][j - 1] == 1)
-			this.qf.union(translateIndex(i, j -1), translateIndex(i, j));
-		
-		if (i < N - 1 && this.grid[i + 1][j] == 1)
-			this.qf.union(translateIndex(i + 1, j), translateIndex(i, j));
-		
-		if (j < N - 1 && this.grid[i][j + 1] == 1)
-			this.qf.union(translateIndex(i, j + 1), translateIndex(i, j));
-		
-	}
-	
-	public boolean isOpen(int i, int j) {
-		checkIndex(i, j);
-		i--;
-		j--;
-		return this.grid[i][j] == 1;
-	}
-	
-	public boolean isFull(int i, int j) {
-		checkIndex(i, j);
-		i--;
-		j--;
-		int N = this.size;
-		return (this.qf.connected(N * N + 1, translateIndex(i, j)));
-	}
-	
-	public boolean percolates() {
-		int N = this.size;
-		return (this.qf.connected(N * N, N * N + 1));
-		
-	}
+    private WeightedQuickUnionUF qf;
+    private int size;
+    private int[][] grid;
+    
+    public Percolation(final int n) {
+        this.size = n;
+        this.qf = new WeightedQuickUnionUF(n * n + 2);
+        int i = 0;
+        this.grid = new int[n][n];
+        
+        for (i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                this.grid[i][j] = 0;
+    }
+    
+    /*
+     * Translate the i, j coordinates to one dimensional space.
+     */
+    private int translateIndex(final int i, final int j) {
+        int n = this.size;
+        return (i * n + j);
+    }
+    
+    /*
+     * Validate indices.
+     */
+    private void checkIndex(final int i, final int j) {
+        int n = this.size;
+        if (i < 1 || i > n)
+            throw new IndexOutOfBoundsException(i + " " + j);
+        
+        if (j < 1 || j > n)
+            throw new IndexOutOfBoundsException(i + " " + j);
+    }
+    
+    /*
+     * Open a site with coordinates(i-1, j-1)
+     */
+    public void open(final int x, final int  y) {
+        checkIndex(x, y);
+        int i = x - 1;
+        int j = y -1;
+        this.grid[i][j] = 1;
+        int n = this.size;
+        if (i == 0) {
+            this.qf.union(translateIndex(i, j), n * n + 1);
+        } else if (i == n - 1) {
+            this.qf.union(translateIndex(i, j), n * n);
+        }
+        
+        if (i > 0 && this.grid[i - 1][j] == 1)
+            this.qf.union(translateIndex(i - 1, j),
+                    translateIndex(i, j));
+        
+        if (j > 0 && this.grid[i][j - 1] == 1)
+            this.qf.union(translateIndex(i, j -1),
+                    translateIndex(i, j));
+        
+        if (i < n - 1 && this.grid[i + 1][j] == 1)
+            this.qf.union(translateIndex(i + 1, j),
+                    translateIndex(i, j));
+        
+        if (j < n - 1 && this.grid[i][j + 1] == 1)
+            this.qf.union(translateIndex(i, j + 1),
+                    translateIndex(i, j));
+        
+    }
+    
+    /*
+     * Check whether the site(i, j) is open or not.
+     */
+    public boolean isOpen(final int i, final int j) {
+        checkIndex(i, j);
+        return this.grid[i - 1][j - 1] == 1;
+    }
+    
+    /*
+     * Check if a site is full or not.
+     */
+    public boolean isFull(final int i, final int j) {
+        checkIndex(i, j);
+        int n = this.size;
+        return (this.qf.connected(n * n + 1, translateIndex(i - 1, j - 1)));
+    }
+    
+    /*
+     * Check if the grid percolates.
+     */
+    public boolean percolates() {
+        int n = this.size;
+        return (this.qf.connected(n * n, n * n + 1));
+        
+    }
 }
--- unionFind/PercolationStats.java
+++ unionFind/PercolationStats.java
@@ -1,72 +1,62 @@
-import java.lang.Math;
-
+/*
+ * Abhinav Upadhyay
+ * September 5th, 2012.
+ * PercolationStats implementation.
+ */
 public class PercolationStats {
-	private int numExperiment;
-	private int size;
-	private double threshHold[];
-	
-	public PercolationStats(int N, int T) {
-		if (N <= 0 || T <= 0)
-			throw new IllegalArgumentException();
-		
-		this.size = N;
-		this.numExperiment = T;
-		this.threshHold = new double[T];
-	}
-	
-	public int getSize() {
-		return this.size;
-	}
-	
-	public int getNumExperiment() {
-		return this.numExperiment;
-	}
-	
-	public double mean() {
-		return StdStats.mean(this.threshHold);
-	}
-	
-	public double stddev() {
-		return StdStats.stddevp(this.threshHold);
-	}
-	
-	public static void main(String[] args) {
-		long start = System.nanoTime();
-		int N = Integer.parseInt(args[0]);
-		int T = Integer.parseInt(args[1]);
-		System.out.println(N + " " + T);
-		Percolation p1 = new Percolation(N);
-		System.out.println(p1.percolates());
-			
-		PercolationStats ps = new PercolationStats(N, T);
-		for (int k = 0; k < T; k++) {
-			Percolation p = new Percolation(N);
-			int count = 0;
-			int table[][] = new int[N][N];
-			while (p.percolates() != true) {
-				int i = StdRandom.uniform(1, N + 1);
-				int j = StdRandom.uniform(1, N + 1);
-				
-				
-				if (table[i-1][j-1] == 1)
-					continue;
-				table[i-1][j-1] = 1;
-				p.open(i, j);
-				count++;
-			}
-			ps.threshHold[k] = (count * 1.0 )/ (N * N);
-			//System.out.println(ps.threshHold[k]);
-		}
-		double mean = ps.mean();
-		double stddev = ps.stddev();
-		long end = System.nanoTime();
-		System.out.println(("mean = " + mean));
-		System.out.println(("stddev =" + stddev));
-		System.out.println((end - start) * Math.pow(10.0, -9.0) + " s");
-	}
-
-	public double[] getThreshhold() {
-		return this.threshHold;
-	}
-	
+    private int numExperiment;
+    private int size;
+    private double[] threshHold;
+    
+    public PercolationStats(int n, int t) {
+        if (n <= 0 || t <= 0)
+            throw new IllegalArgumentException();
+        
+        this.size = n;
+        this.numExperiment = t;
+        this.threshHold = new double[t];
+    }
+    
+    
+    /*
+     * Return the mean
+     */
+    public double mean() {
+        return StdStats.mean(this.threshHold);
+    }
+    
+    /*
+     * Return the standard deviation.
+     */
+    public double stddev() {
+        return StdStats.stddev(this.threshHold);
+    }
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+             
+        PercolationStats ps = new PercolationStats(n, t);
+        for (int k = 0; k < t; k++) {
+            Percolation p = new Percolation(n);
+            int count = 0;
+            int[][] table = new int[n][n];
+            while (!p.percolates()) {
+                int i = StdRandom.uniform(1, n + 1);
+                int j = StdRandom.uniform(1, n + 1);
+                
+                if (table[i-1][j-1] == 1)
+                    continue;
+                table[i-1][j-1] = 1;
+                p.open(i, j);
+                count++;
+            }
+            ps.threshHold[k] = (count * 1.0)/(n * n);
+        }
+ 
+        double mean = ps.mean();
+        double stddev = ps.stddev();
+        System.out.println(("mean = " + mean));
+        System.out.println(("stddev =" + stddev));
+    }
 }
